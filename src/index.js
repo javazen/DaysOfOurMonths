0d6dc0ac15613c9cd1c5890b6548301b83bdb376
Sam Ragucci <samr742@gmail.com>
2020-07-18 10:18:57 -0700

    add analyze button

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import {process} from './days.js';
 
-let leapCB, totalDaysInput;
-let mTotal, mTotalNeeded;
+let leapCB, totalDaysInput, analyze;
+let mTotal, mTotalNeeded, correctTotal;
 const monthsInputs = {};
-const monthsObj = {m31:7, m30:4, m29:0, m28:1, leapValue:false};
+const monthsObj = {m31:6, m30:5, m29:0, m28:1, leapValue:false};
 
 document.addEventListener("DOMContentLoaded", function() {
   
   ['m31', 'm30', 'm29', 'm28'].forEach( (item) => {
     monthsInputs[item] = document.getElementById(item);
-    monthsInputs[item].setAttribute("value", monthsObj[item]);
+    monthsInputs[item].value = monthsObj[item];
     addListeners(monthsInputs[item]);
   });
   
@@ -17,23 +17,27 @@ document.addEventListener("DOMContentLoaded", function() {
   leapCB = document.getElementById("leap");
   leapCB.checked = monthsObj.leapValue;
   leapCB.addEventListener('change', clickLeapCB);
-  updateTotal(monthsObj);
+  correctTotal = updateTotal(monthsObj);
+  analyze = document.getElementById("analyze");
+  analyze.addEventListener('click', handleAnalyze);
   
-  let non31array = [];
-  for (let i=0; i<monthsObj.m30; i++) non31array.push(30);
-  for (let i=0; i<monthsObj.m29; i++) non31array.push(29);
-  for (let i=0; i<monthsObj.m28; i++) non31array.push(28);
-  const obj = process(non31array, leapCB.checked); // [28,30,30,30,30]
-  
-  updateOutput(obj);
+  if (correctTotal) handleAnalyze();
 });
 
 function clickInput() {
   this.select();
 }
+function handleAnalyze() {
+  let non31array = [];
+  for (let i=0; i<monthsObj.m30; i++) non31array.push(30);
+  for (let i=0; i<monthsObj.m29; i++) non31array.push(29);
+  for (let i=0; i<monthsObj.m28; i++) non31array.push(28);
+  const obj = process(non31array, leapCB.checked); // [28,30,30,30,30]
+  updateOutput(obj);
+}
 function updateValue(e) {
   monthsObj[this.getAttribute("id")] = +e.target.value;
-  updateTotal(monthsObj);
+  correctTotal = updateTotal(monthsObj);
 }
 function addListeners(elem) {
   elem.addEventListener('click', clickInput);
@@ -41,35 +45,36 @@ function addListeners(elem) {
 }
 function clickLeapCB() {
   monthsObj.leapValue = (this.checked);
-  updateTotal(monthsObj);
+  correctTotal = updateTotal(monthsObj);
 }
 
 function updateTotal(monthsObj) {
   mTotalNeeded = (monthsObj.leapValue) ? 366 : 365;
   mTotal = monthsObj.m31*31 + monthsObj.m30*30 + monthsObj.m29*29 + monthsObj.m28*28;
   totalDaysInput.innerText = "" + mTotal;
-  const wrongTotal = (mTotal !==  mTotalNeeded);
-  if (wrongTotal)
-    totalDaysInput.classList.add("wrongTotalColoring");
-  else
+  const correctTotal = (mTotal === mTotalNeeded);
+  if (correctTotal)
     totalDaysInput.classList.remove("wrongTotalColoring");
+  else
+    totalDaysInput.classList.add("wrongTotalColoring");
+  return correctTotal;
 }
 
 function updateOutput(outputObj) {
   const lowScoreInput = document.getElementById("lowScore");
-  lowScoreInput.setAttribute("value", outputObj.lowScore);
+  lowScoreInput.value = outputObj.lowScore;
   const lowScoreExample = document.getElementById("lowScoreExample");
   let lowExStr = outputObj.lowScoreExample.join(", ");
-  lowScoreExample.setAttribute("value", lowExStr);
+  lowScoreExample.value = lowExStr;
   
   const highScoreInput = document.getElementById("highScore");
-  highScoreInput.setAttribute("value", outputObj.highScore);
+  highScoreInput.value = outputObj.highScore;
   const highScoreExample = document.getElementById("highScoreExample");
   let highExStr = outputObj.highScoreExample.join(", ");
-  highScoreExample.setAttribute("value", highExStr);
+  highScoreExample.value = highExStr;
   
   const scoreCounts = document.getElementById("scoreCounts");
   let scoreCountsStr = outputObj.scoreCounts.join(", ");
-  scoreCounts.setAttribute("value", scoreCountsStr);
+  scoreCounts.value = scoreCountsStr;
 }
 
